d5e6181609aa46499b26ad44e5e08036e780908f
AbdulAwalNadim <nadim.ice.nstu@gmail.com>
2020-11-13 20:46:37 +0600

    Adding new files

---
+++ Logging in Python/Logging Cookbook/Customizing LogRecord/18.py
@@ -0,0 +1,8 @@
+old_factory = logging.getLogRecordFactory()
+
+def record_factory(*args, **kwargs):
+    record = old_factory(*args, **kwargs)
+    record.custom_attribute = 0xdecafbad
+    return record
+
+logging.setLogRecordFactory(record_factory)
